302d53e424e013243da972e6dbf8cacf2d770ad2
laceous <laceous@gmail.com>
2023-07-24 02:31:03 -0700

    add eden-tokens

--- win streak shenanigans/main.lua
+++ win streak shenanigans/main.lua
@@ -2,13 +2,15 @@ local mod = RegisterMod('Win Streak Shenanigans', 1)
 local game = Game()
 
 -- usage: win-streak +10
+-- usage: eden-tokens +10
 function mod:onExecuteCmd(cmd, parameters)
+  local seeds = game:GetSeeds()
+  local level = game:GetLevel()
+  local room = level:GetCurrentRoom()
+  local max = 9999 -- 4 digits
   cmd = string.lower(cmd)
   
   if cmd == 'win-streak' then
-    local seeds = game:GetSeeds()
-    local max = 9999 -- 4 digits
-    
     if not seeds:IsCustomRun() then -- not challenge or seeded run
       if string.len(parameters) >= 2 and string.sub(parameters, 1, 1) == '+' then
         local num = tonumber(string.sub(parameters, 2))
@@ -17,6 +19,30 @@ function mod:onExecuteCmd(cmd, parameters)
           for i = 1, num do
             game:End(2) -- Mom / Epilogue cutscene
           end
+          
+          print('+' .. num)
+          return
+        end
+      end
+    end
+    
+    print('+0')
+  elseif cmd == 'eden-tokens' then
+    if not game:IsGreedMode() and not seeds:IsCustomRun() then -- not greed mode, challenge, or seeded run
+      if string.len(parameters) >= 2 and string.sub(parameters, 1, 1) == '+' then
+        local num = tonumber(string.sub(parameters, 2))
+        
+        if math.type(num) == 'integer' and num > 0 and num <= max then
+          Isaac.ExecuteCommand('stage 8') -- womb 2
+          
+          level.LeaveDoor = DoorSlot.NO_DOOR_SLOT
+          game:ChangeRoom(level:GetRooms():Get(level:GetLastBossRoomListIndex()).SafeGridIndex, -1) -- mom's heart / it lives
+          
+          for i = 1, num do
+            room:TriggerClear(true)
+          end
+          
+          game:End(3) -- Mom's Heart endings
           
           print('+' .. num)
           return
